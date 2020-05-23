a5c01268151f3fd44c1510fed3ca07d1bce0ab01
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 13:24:19 +0100

    Update start.sh Logic

--- build/start.sh
+++ build/start.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+# !! Run this script from the build directory !!
 
 FILE=../Dockerfile
+NAME="template"
 
-if [ -f "$FILE" ]; then
-  cd ..
+echo 'Trying to find Dockerfile'
+if [ -f "$FILE" ]; 
+  then
+    echo 'Found Dockerfile, navigating to directory.'
+    cd ..
+  else
+    echo 'Could not find Dockerfile. Please navigate to the build directory and run this script.' 
+    exit
 fi
 
-# Stop any existing containers.
-docker container stop template
+if [ "$(docker inspect -f '{{.State.Running}}' "$NAME")" == "true" ];
+  then
+    echo 'Stopping existing Docker containers.'
+    docker container stop $NAME
+    
+    echo 'Removing existing Docker containers.'
+    docker container rm $NAME
+fi
 
-# Remove any existing containers.
-docker container rm template
+if [ ! "$(docker ps -q -f name=$NAME)" ]; 
+  then
+    if [ "$(docker ps -aq -f status=exited -f name=$NAME)" ]; 
+      then
+        docker container rm $NAME
+    fi
+fi
 
-# Build a new container called 
-docker build -t template -f ./Dockerfile .
+echo 'Building Docker image.'
+docker build -t $NAME -f ./Dockerfile .
 
-docker run --network host --restart on-failure:5 --name "template" -d template
+echo 'Running Docker process in a container.'
+docker run --network host --restart on-failure:5 --name $NAME -d $NAME
