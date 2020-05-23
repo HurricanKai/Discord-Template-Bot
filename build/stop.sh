afae466bbd96bd635143ae4cf7f2d99558d3f00d
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 13:42:30 +0100

    Update stop.sh Logic

--- build/stop.sh
+++ build/stop.sh
@@ -1,3 +1,19 @@
-docker container stop template
+NAME="template"
 
-docker container rm template
+if [ "$(docker inspect -f '{{.State.Running}}' "$NAME")" == "true" ];
+  then
+    echo 'Existing running Docker container found.'
+    
+    echo 'Stopping Docker container.'
+    docker container stop $NAME
+    
+    echo 'Removing Docker container.'
+    docker container rm $NAME
+fi
+
+if [ "$(docker ps -f name=$NAME)" ]; 
+  then
+    echo 'Non-running Docker container found.'
+    echo 'Removing Docker container.'
+    docker container rm $NAME
+fi
