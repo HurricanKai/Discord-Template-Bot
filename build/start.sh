c01cd969ca83d3f3e8a21d70128599298e728b8c
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 12:32:20 +0100

    Update start.sh Logic

--- build/start.sh
+++ build/start.sh
@@ -1,9 +1,18 @@
-cd ..
+#!/bin/bash
 
+FILE=../Dockerfile
+
+if [ -f "$FILE" ]; then
+  cd ..
+fi
+
+# Stop any existing containers.
 docker container stop template
 
+# Remove any existing containers.
 docker container rm template
 
+# Build a new container called 
 docker build -t template -f ./Dockerfile .
 
 docker run --network host --restart on-failure:5 --name "template" -d template
