2c44bed32f5dc725ef01a3e79d7029eace4d2f38
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 12:14:48 +0100

    Create Database Service Implementation Extension Method

---
+++ Template.Data/TemplateContextSetup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Template.Data
+{
+    public static class TemplateContextSetup
+    {
+        public static IServiceCollection ConfigureTemplateContext(this IServiceCollection serviceCollection, string connectionString)
+            => serviceCollection.AddDbContext<TemplateContext>(options => options.UseNpgsql(connectionString));
+    }
+}
