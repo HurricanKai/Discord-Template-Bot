90719866c9bba693be9e0424e1074c87b95b6456
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 13:41:45 +0100

    Finalize start.sh Script

--- build/start.sh
+++ build/start.sh
@@ -16,15 +16,19 @@ fi
 
 if [ "$(docker inspect -f '{{.State.Running}}' "$NAME")" == "true" ];
   then
-    echo 'Stopping existing Docker containers.'
+    echo 'Existing running Docker container found.'
+    
+    echo 'Stopping Docker container.'
     docker container stop $NAME
     
-    echo 'Removing existing Docker containers.'
+    echo 'Removing Docker container.'
     docker container rm $NAME
 fi
 
-if [ "$(docker ps -q -f name=$NAME)" ]; 
+if [ "$(docker ps -f name=$NAME)" ]; 
   then
+    echo 'Non-running Docker container found.'
+    echo 'Removing Docker container.'
     docker container rm $NAME
 fi
 
