ac69c3e4bf7d0afed39b6feb699c0cf915675679
Matthew Trip <matthewtrip2@gmail.com>
2020-05-23 13:37:04 +0100

    Update start.sh Logic

--- build/start.sh
+++ build/start.sh
@@ -25,10 +25,7 @@ fi
 
 if [ "$(docker ps -q -f name=$NAME)" ]; 
   then
-    if [ "$(docker ps -aq -f status=exited -f name=$NAME)" ]; 
-      then
-        docker container rm $NAME
-    fi
+    docker container rm $NAME
 fi
 
 echo 'Building Docker image.'
